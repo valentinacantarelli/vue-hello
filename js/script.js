e735b4668af6511693e736d167a5d2ae7a9cd94c
valentina cantarelli <valentinacanterina@gmail.com>
2021-09-16 18:57:39 +0200

    aggiornata selezione sesso

--- js/script.js
+++ js/script.js
@@ -6,10 +6,15 @@ const app=new Vue(
     el:"#root",
     data:{
         miaClasse:["titolo","sottotitolo"],
-        messaggio:"Bentornat*",        
+        messaggio:["Bentornata","Bentornato"],
+        sesso:prompt("Sei maschio o femmina?"),
         name:prompt("Inserisci il tuo nome"),
         linkImg:'https://images.unsplash.com/photo-1531877025030-f7696a50770f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80'
+    },
+    methods:{
+        selezionaSesso:function(){
+            (this.sesso == "maschio") ? this.message[1] : this.messaggio[0]
+        }
     }
-    
 }
 )

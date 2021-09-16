7deda16429ad1834aa41958338996a5de1ba8519
valentina cantarelli <valentinacanterina@gmail.com>
2021-09-16 14:44:57 +0200

    esercizio vue

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// Aggiungere alla pagina un’immagine, presa anch’essa da un data
+
+const app=new Vue(
+    {
+    el:"#root",
+    data:{
+        messaggio:"Bentornato",
+        linkImg:'https://images.unsplash.com/photo-1531877025030-f7696a50770f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80'
+    }
+}
+)

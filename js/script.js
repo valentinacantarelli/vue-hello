1bb51f51db27a4c869d80d4aa374a81ac7ce08df
valentina cantarelli <valentinacanterina@gmail.com>
2021-09-16 16:18:16 +0200

    aggiornato esercizio

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ const app=new Vue(
     {
     el:"#root",
     data:{
+        miaClasse:["titolo","sottotitolo"],
         messaggio:"Bentornata",
         name:prompt("Inserisci il tuo nome"),
         linkImg:'https://images.unsplash.com/photo-1531877025030-f7696a50770f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80'
